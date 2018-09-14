b00d27bcecb0ad71ad87258fb0b32f5838041889
Alberto Buffolino <a.buffolino@gmail.com>
2018-09-14 22:31:49 +0200

    Updated version and package.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Add-on to facilitate toolbars management."""),
 	# version
-	"addon_version" : "1.0-20180912-dev",
+	"addon_version" : "1.0-20180914-dev",
 	# Author(s)
 	"addon_author" : u"Alberto Buffolino <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
