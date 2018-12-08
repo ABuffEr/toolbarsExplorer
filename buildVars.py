f6ee96453748a5b659e8f7d3fe1e8395397a2a38
Alberto Buffolino <a.buffolino@gmail.com>
2018-12-08 14:33:18 +0100

    Prepare for 1.1 release with new compatibility keys in manifest.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Add-on to facilitate toolbars management."""),
 	# version
-	"addon_version" : "1.0-20181208-dev",
+	"addon_version" : "1.1",
 	# Author(s)
 	"addon_author" : u"Alberto Buffolino <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
@@ -31,7 +31,7 @@ addon_info = {
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion" : "2019.2",
 	# Add-on update channel (default is stable or None)
-	"addon_updateChannel" : "dev",
+	"addon_updateChannel" : "stable",
 }
 
 import os.path
