3919e8e00ce65d3bf6299a0492529a38c668606f
Alberto Buffolino <a.buffolino@gmail.com>
2018-12-08 14:43:39 +0100

    Fix version to 1.1-yyyymmdd-dev.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Add-on to facilitate toolbars management."""),
 	# version
-	"addon_version" : "1.0-20181208-dev",
+	"addon_version" : "1.1-20181208-dev",
 	# Author(s)
 	"addon_author" : u"Alberto Buffolino <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
